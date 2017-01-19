a7676e898f9a2180f901d25a8407ed33d019ea1e
D'ontreye Nero <dontreyenero@gmail.com>
2017-01-19 18:31:19 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
 	let id = req.params.id;
 
-	if(!ObjectID.isValid(id)) {
+	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
@@ -47,6 +47,24 @@ app.get('/todos/:id', (req, res) => {
 		res.send({todo});
 	}).catch((e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.delete('/todos/:id', (req, res) => {
+	let id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send();
+		}
+
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
