453abb2313d03b4dd1f34568cfcd0c03f78ca642
D'ontreye Nero <dontreyenero@gmail.com>
2017-01-21 08:22:58 -0500

    Add POST /users route and token auth

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,64 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		trim: true,
 		required: true,
-		minlength: 1
-	}
+		minlength: 6,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
+
+
+// determines what gets sent back when a mongoose model is converted to JSON
+UserSchema.methods.toJSON = function () {
+	let user = this;
+	let userObject = user.toObject(); // convert your mongoose var 'user' into a regular obj
+	
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+// add a method to the UserSchema to generate the auth token
+// not using an arrow fn because 'this' stores a ref to the individual user document
+UserSchema.methods.generateAuthToken = function () {
+	let user = this;
+	let access = 'auth';
+	let token = jwt.sign({_id: user._id.toHexString(), access}, 'indeed').toString();
+
+	user.tokens.push({
+		access,
+		token
+	});
+
+	return user.save().then(() => {
+		return token;
+	});
+};
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {
 	User
