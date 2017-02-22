9aaabc1d7faa338d54ba852f195d466b5d953af7
D'ontreye Nero <dontreyenero@gmail.com>
2017-02-22 07:31:42 -0500

    Add read test using find

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-//mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 before((done) => {
 	mongoose.connect('mongodb://localhost/users_test'); // specify the db to connect to
 	mongoose.connection
-		.once('open', () => { done(); }) // only run this event once
+		.once('open', () => {
+			done();
+		}) // only run this event once
 		.on('error', (error) => { // run this event whenever an error event happens
 			console.warn('Warning', error);
 		});
