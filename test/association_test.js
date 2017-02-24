1dbe49c38ca07147e1507cd9be4b8e9819c853e1
D'ontreye Nero <dontreye.nero@hdsupply.com>
2017-02-23 19:09:03 -0500

    Associate all schemas and log results

--- test/association_test.js
+++ test/association_test.js
@@ -4,5 +4,26 @@ const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Associations', () => {
+	let joe, blogPost, comment;
+	
+	beforeEach((done) => {
+		joe = new User({name: 'Joe'});
+		blogPost = new BlogPost({title: 'JS is great', content: 'Indeed, it is'});
+		comment = new Comment({content: 'Congrats on your post'});
 
+		joe.blogPosts.push(blogPost); // add the blog post to the user Joe
+		blogPost.comments.push(comment); // add comment to blog post
+		comment.user = joe; // make Joe the user that made this comment
+
+		Promise.all([joe.save(), blogPost.save(), comment.save()])
+			.then(() => done());
+	});
+
+	it.only('saves a relation between a user and a blogpost', (done) => {
+		User.findOne({ name: 'Joe' })
+			.then((user) => {
+				console.log(user);
+				done();
+			});
+	});
 });
